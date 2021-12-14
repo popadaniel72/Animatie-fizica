ce957cbc8935eaa4bf72f1f41fbbdff8e5f8f7d0
popadaniel72 <popadaniel72@yahoo.com>
2021-12-14 09:58:45 +0200

    Add project files.

---
+++ Desenare_linii_fizica/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace Desenare_linii_fizica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ValUnghi = new System.Windows.Forms.HScrollBar();
+            this.AfUnghi = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.AfForta = new System.Windows.Forms.Label();
+            this.ValForta = new System.Windows.Forms.HScrollBar();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.AfMiu = new System.Windows.Forms.Label();
+            this.ValMiu = new System.Windows.Forms.HScrollBar();
+            this.PtMasa = new System.Windows.Forms.GroupBox();
+            this.AfMasa = new System.Windows.Forms.Label();
+            this.ValMasa = new System.Windows.Forms.HScrollBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.PtMasa.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.AfUnghi);
+            this.groupBox1.Controls.Add(this.ValUnghi);
+            this.groupBox1.Location = new System.Drawing.Point(15, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(318, 96);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Unghi";
+            // 
+            // ValUnghi
+            // 
+            this.ValUnghi.Location = new System.Drawing.Point(3, 22);
+            this.ValUnghi.Maximum = 90;
+            this.ValUnghi.Name = "ValUnghi";
+            this.ValUnghi.Size = new System.Drawing.Size(312, 18);
+            this.ValUnghi.TabIndex = 1;
+            this.ValUnghi.Scroll += new System.Windows.Forms.ScrollEventHandler(this.ValUnghi_Scroll);
+            // 
+            // AfUnghi
+            // 
+            this.AfUnghi.AutoSize = true;
+            this.AfUnghi.Location = new System.Drawing.Point(163, 58);
+            this.AfUnghi.Name = "AfUnghi";
+            this.AfUnghi.Size = new System.Drawing.Size(18, 20);
+            this.AfUnghi.TabIndex = 1;
+            this.AfUnghi.Text = "0";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.AfForta);
+            this.groupBox2.Controls.Add(this.ValForta);
+            this.groupBox2.Location = new System.Drawing.Point(342, 15);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Size = new System.Drawing.Size(318, 96);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Forța";
+            // 
+            // AfForta
+            // 
+            this.AfForta.AutoSize = true;
+            this.AfForta.Location = new System.Drawing.Point(163, 58);
+            this.AfForta.Name = "AfForta";
+            this.AfForta.Size = new System.Drawing.Size(18, 20);
+            this.AfForta.TabIndex = 1;
+            this.AfForta.Text = "0";
+            // 
+            // ValForta
+            // 
+            this.ValForta.Location = new System.Drawing.Point(3, 22);
+            this.ValForta.Maximum = 90;
+            this.ValForta.Name = "ValForta";
+            this.ValForta.Size = new System.Drawing.Size(312, 18);
+            this.ValForta.TabIndex = 1;
+            this.ValForta.Scroll += new System.Windows.Forms.ScrollEventHandler(this.ValForta_Scroll);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.AfMiu);
+            this.groupBox3.Controls.Add(this.ValMiu);
+            this.groupBox3.Location = new System.Drawing.Point(667, 15);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox3.Size = new System.Drawing.Size(318, 96);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Coeficient de frecare";
+            // 
+            // AfMiu
+            // 
+            this.AfMiu.AutoSize = true;
+            this.AfMiu.Location = new System.Drawing.Point(163, 58);
+            this.AfMiu.Name = "AfMiu";
+            this.AfMiu.Size = new System.Drawing.Size(18, 20);
+            this.AfMiu.TabIndex = 1;
+            this.AfMiu.Text = "0";
+            // 
+            // ValMiu
+            // 
+            this.ValMiu.Location = new System.Drawing.Point(3, 22);
+            this.ValMiu.Maximum = 90;
+            this.ValMiu.Name = "ValMiu";
+            this.ValMiu.Size = new System.Drawing.Size(312, 18);
+            this.ValMiu.TabIndex = 1;
+            this.ValMiu.Scroll += new System.Windows.Forms.ScrollEventHandler(this.ValMiu_Scroll);
+            // 
+            // PtMasa
+            // 
+            this.PtMasa.Controls.Add(this.AfMasa);
+            this.PtMasa.Controls.Add(this.ValMasa);
+            this.PtMasa.Location = new System.Drawing.Point(993, 15);
+            this.PtMasa.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.PtMasa.Name = "PtMasa";
+            this.PtMasa.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.PtMasa.Size = new System.Drawing.Size(318, 96);
+            this.PtMasa.TabIndex = 3;
+            this.PtMasa.TabStop = false;
+            this.PtMasa.Text = "Masa";
+            // 
+            // AfMasa
+            // 
+            this.AfMasa.AutoSize = true;
+            this.AfMasa.Location = new System.Drawing.Point(163, 58);
+            this.AfMasa.Name = "AfMasa";
+            this.AfMasa.Size = new System.Drawing.Size(18, 20);
+            this.AfMasa.TabIndex = 1;
+            this.AfMasa.Text = "0";
+            // 
+            // ValMasa
+            // 
+            this.ValMasa.Location = new System.Drawing.Point(3, 22);
+            this.ValMasa.Maximum = 90;
+            this.ValMasa.Minimum = 1;
+            this.ValMasa.Name = "ValMasa";
+            this.ValMasa.Size = new System.Drawing.Size(312, 18);
+            this.ValMasa.TabIndex = 1;
+            this.ValMasa.Value = 1;
+            this.ValMasa.Scroll += new System.Windows.Forms.ScrollEventHandler(this.ValMasa_Scroll);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DimGray;
+            this.button1.Location = new System.Drawing.Point(18, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Animatie";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DimGray;
+            this.button2.Location = new System.Drawing.Point(144, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 32);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(1332, 642);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PtMasa);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ForeColor = System.Drawing.Color.Yellow;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Miscare corp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.PtMasa.ResumeLayout(false);
+            this.PtMasa.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.HScrollBar ValUnghi;
+        private System.Windows.Forms.Label AfUnghi;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label AfForta;
+        private System.Windows.Forms.HScrollBar ValForta;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label AfMiu;
+        private System.Windows.Forms.HScrollBar ValMiu;
+        private System.Windows.Forms.GroupBox PtMasa;
+        private System.Windows.Forms.Label AfMasa;
+        private System.Windows.Forms.HScrollBar ValMasa;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Desenare_linii_fizica/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desenare_linii_fizica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int baza, lung, stg = 10;
+        Pen rosu = new Pen(Color.Red, 1);
+        Pen verde = new Pen(Color.Green, 1);
+        Pen albastru = new Pen(Color.Blue, 1);
+        Pen galben = new Pen(Color.Yellow, 1);
+        double accX, accY, vitX, vitY, miu, masa, ag=9.81;
+        int forta, unghi, lu = 50, la = 30, x, y, dv=10;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            x = 2 * stg;
+            y = baza - 1;
+            accX = accY = vitX = vitY = 0;
+            masa = ValMasa.Value;
+            unghi = ValUnghi.Value;
+            forta = ValForta.Value;
+            this.Invalidate();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (accX > 0)
+            {
+                vitX += accX * (1.0 / timer1.Interval)*dv;
+                x += (int)(vitX * (1.0 / timer1.Interval));
+            }
+            if (accY > 0)
+            {
+                vitY += accY * (1.0 / timer1.Interval)*dv;
+                y -= (int)(vitY * (1.0 / timer1.Interval));
+            }
+            this.Invalidate();
+            if (x > this.Width || y<20) timer1.Enabled = false;
+                   }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            accX = (forta * Math.Cos(unghi * Math.PI / 180) - masa * miu * ag)/masa;
+            accY = (forta * Math.Sin(unghi * Math.PI / 180) - masa * ag) / masa;
+            vitX = vitY = 0;
+            if (accX <= 0 && accY <= 0) MessageBox.Show("Nu se mișcă ");
+            else timer1.Enabled = true;
+        }
+
+        private void ValMasa_Scroll(object sender, ScrollEventArgs e)
+        {
+            AfMasa.Text = (ValMasa.Value / 10.0).ToString() + " Kg";
+            masa = ValMasa.Value / 10.0;
+            this.Invalidate();
+        }
+
+        private void ValMiu_Scroll(object sender, ScrollEventArgs e)
+        {
+            AfMiu.Text = (ValMiu.Value / 100.0).ToString();
+            miu = ValMiu.Value / 100.0;
+            this.Invalidate();
+        }
+
+        private void ValForta_Scroll(object sender, ScrollEventArgs e)
+        {
+            AfForta.Text = ValForta.Value.ToString()+" N";
+            forta = ValForta.Value;
+            this.Invalidate();
+        }
+
+        
+        private void ValUnghi_Scroll(object sender, ScrollEventArgs e)
+        {
+            AfUnghi.Text = ValUnghi.Value.ToString();
+            unghi = ValUnghi.Value;
+            this.Invalidate();
+        }
+
+        
+        // x,y= punct de aplicare, u-unghi, l-lungime vector, cul-culoare, e- suport desen
+        void dVector(int x, int y, int u, int l, Pen cul, PaintEventArgs e)
+        {
+            int a = 30, vv = 10; //a = unghiul varfului, vv= lungimea varf
+            double dx, dy;
+            int xv, yv;
+            // desen linie principala
+            dx = l * Math.Cos(u * Math.PI / 180);
+            dy = l * Math.Sin(u * Math.PI / 180);
+            xv = x + (int)dx; // xv, yv - coordonatele varfului
+            yv = y - (int)dy;
+            e.Graphics.DrawLine(cul, x, y, xv , yv );
+            // desen varf
+            dx = vv * Math.Cos((u + 180 - a) * Math.PI / 180);
+            dy = vv * Math.Sin((u + 180 - a) * Math.PI / 180);
+            e.Graphics.DrawLine(cul, xv, yv, xv + (int)dx, yv - (int)dy);
+            dx = vv * Math.Cos((u + 180 + a) * Math.PI / 180);
+            dy = vv * Math.Sin((u + 180 + a) * Math.PI / 180);
+            e.Graphics.DrawLine(cul, xv, yv, xv + (int)dx, yv - (int)dy);
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            baza = this.Height - 50;
+            lung = this.Width - 3 * stg;
+            this.Invalidate();
+            x = 2 * stg;
+            y = baza-1;
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            baza = this.Height - 50;
+            lung = this.Width - 3 * stg;
+            x = 2 * stg;
+            y = baza -1;
+            accX = accY = vitX = vitY = 0;
+            masa = 0.1;
+        }
+
+        
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // desenez baza
+            e.Graphics.DrawLine(albastru, stg, baza, lung, baza);
+            // desenez corpul
+            e.Graphics.DrawRectangle(verde, x, y-la, lu,  la);
+            // desenez vectorul forta
+            dVector(x + lu / 2, y - la / 2, unghi, forta, rosu, e);
+            //desenez vectorul forta de frecare
+            dVector(x + lu / 2, y - la / 2, 180, (int)(miu*ag*masa), galben, e);
+        }
+    }
+}
